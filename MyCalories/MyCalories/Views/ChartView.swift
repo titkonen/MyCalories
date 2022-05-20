a95a9c5ffe8451144edaf0ac260e0ded40744d46
Toni Itkonen <toni.itkonen@icloud.com>
2022-05-20 13:07:09 +0300

    + Add: ChartView with mockup data

--- MyCalories/MyCalories/Views/ChartView.swift
+++ MyCalories/MyCalories/Views/ChartView.swift
@@ -1,9 +1,86 @@
 import SwiftUI
 
-struct ChartView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Bar: Identifiable {
+  let id = UUID()
+  var name: String
+  var day: String
+  var value: Double
+  var color: Color
+  
+  static var sampleBars: [Bar] {
+    var tempBars = [Bar]()
+    var color: Color = .green
+    let days = ["M", "T", "W", "T", "F", "S", "S"]
+  
+    
+    for week in 1...7 {
+      let randomValue = Double.random(in: 20...200)
+      
+      if randomValue > 150 {
+        color = .red
+      } else if randomValue > 75 {
+        color = .yellow
+      } else {
+        color = .green
+      }
+      
+      ///Single bar block
+      let bar = Bar(name: "\(week)", day: days[week-1], value: randomValue, color: color)
+      tempBars.append(bar)
+      
     }
+    return tempBars
+  }
+}
+
+struct ChartView: View {
+  @State private var bars = Bar.sampleBars
+  @State private var selectedID: UUID = UUID()
+  @State private var text = "Info"
+  
+  var body: some View {
+    VStack {
+      Text(text)
+        .bold()
+        .padding()
+      
+      HStack(alignment: .bottom) {
+        ForEach(bars) { bar in
+          VStack {
+            ZStack {
+              Rectangle()
+                .foregroundColor(bar.color)
+                .frame(width: 35, height: bar.value, alignment: .bottom)
+                .cornerRadius(6)
+                .opacity(selectedID == bar.id ? 0.5 : 1.0)
+                .onTapGesture {
+                  self.selectedID = bar.id
+                  self.text = "Value: \(Int(bar.value))"
+                }
+              Text("\(Int(bar.value))")
+                .foregroundColor(.white)
+            }
+            Text(bar.day)
+          }
+        }
+      }
+      .frame(height: 240, alignment: .bottom)
+      .padding(20)
+      .background(.thinMaterial)
+      .cornerRadius(12)
+      
+      Button("Refresh") {
+        withAnimation {
+          self.bars = Bar.sampleBars
+        }
+      }
+      .padding()
+      
+      Spacer()
+    }
+
+
+  }///-view
 }
 
 struct ChartView_Previews: PreviewProvider {
